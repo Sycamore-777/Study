681315fcd7aeb089e588c01c1ee6009e1ddc2c7b
Sycamore-777 <19521388066@163.com>
2025-12-10 09:46:59 +0800

    修复了call_example.py中没有关闭客户端的bug

--- matlab2python/add_func.m
+++ matlab2python/add_func.m
@@ -3,9 +3,3 @@ function [status,result] = add_func(a, b)
 status = 0;
 result = a + b;
 end
-
-function [status,result] = min_func(a, b)
-% example - A simple function to find the minimum of two numbers.
-status = 0;
-result = a - b;
-end
